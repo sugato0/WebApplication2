901380c33e058be7244c13a18b9a0b8aede8f6d8
Sugato0 <sugatokavary@gmail.com>
2024-03-16 16:56:49 +0200

    Добавьте файлы проекта.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public static List<Products> names = new List<Products>() { };
+
+        public static List<Products> buscet_products = new List<Products>() { };
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+
+            names.Add(new Products
+            {
+                Id = 0,
+                Name = "Кофе",
+                Price = 300,
+                description = "Очень вкусный кофе"
+            });
+            names.Add(new Products
+            {
+                Id = 1,
+                Name = "Чай",
+                Price = 200,
+                description = "Чай как чай"
+            });
+
+
+
+            return View(names);
+        }
+        // Home/basket
+
+        public RedirectResult bascet_add(int id)
+        {
+             
+            buscet_products.Add(names[id]);
+            return Redirect("/");
+        }
+        public IActionResult bascet()
+        {
+            ViewData["sale"] = "Все по 50 рублей";
+            return View(buscet_products);
+        }
+        public RedirectResult bascet_delete(int id)
+        {
+            buscet_products.RemoveAt(id);
+            return Redirect("/Home/bascet");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication2/Models/Products.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication2.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string description { get; set; }
+    }
+}
